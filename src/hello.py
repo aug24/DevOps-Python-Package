a131c697517fa823e7000f2584805c9adadde060
Justin Rowles <justin.rowles@bgch.co.uk>
2014-08-21 12:05:46 +0100

    Sorted out bottle framework.

--- src/hello.py
+++ src/hello.py
@@ -1,9 +1,39 @@
 #!/usr/bin/python
-from bottle import route
+import argparse
+import yaml
+from bottle import route, run, static_file
+import logging
 
-def hello(config, logging):
-  @route('/')
-  def HelloWorld(env):
-    logging.log('Asked for a message')
-    return config.message
+parser = argparse.ArgumentParser(description='Hello World')
+parser.add_argument("--config", required=True)
+parser.add_argument("--logging", required=True)
+args = parser.parse_args()
 
+class hello:
+   def __init__(self, config, logconfig):
+      self.config=config
+      self.logger = logging.getLogger(logconfig["name"])
+      handler = logging.FileHandler(logconfig["filename"])
+      formatter = logging.Formatter(logconfig["format"])
+      handler.setFormatter(formatter)
+      self.logger.addHandler(handler) 
+      self.logger.setLevel(logging.INFO)
+   def sayhello(self):
+      self.logger.info('Asked for a message')
+      return config["message"]
+   def start(self):
+      run(host=self.config["host"], port=self.config["port"], debug=True)
+
+logconfig = yaml.load(open(args.logging, 'r'))
+config = yaml.load( open( args.config, 'r'))
+helloapp=hello(config, logconfig)
+
+@route('/favicon.ico')
+def favicon():
+   return static_file('static/favicon.ico', root='.')
+
+@route('/')
+def hello():
+   return helloapp.sayhello()
+
+helloapp.start()
